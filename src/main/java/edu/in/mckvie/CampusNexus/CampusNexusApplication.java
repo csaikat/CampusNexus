ca7a4101ca518c21397d66a07af81c62e31e1743
paul-monish <monish.paul2000@gmail.com>
2023-03-05 09:42:49 +0530

    v1: create:campusnexus setup

---
+++ src/main/java/edu/in/mckvie/CampusNexus/CampusNexusApplication.java
@@ -0,0 +1,15 @@
+package edu.in.mckvie.CampusNexus;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusNexusApplication {
+//	public static Logger logger= LoggerFactory.getLogger(CampusNexusApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(CampusNexusApplication.class, args);
+//		logger.info("application started");
+	}
+
+}
---
+++ src/test/java/edu/in/mckvie/CampusNexus/CampusNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.in.mckvie.CampusNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
